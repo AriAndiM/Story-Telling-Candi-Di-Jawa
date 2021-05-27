bb3064bbf6b21338b021195fb084c0b95684f521
UswatunChasanah <83987071+UswatunChasanah@users.noreply.github.com>
2021-05-27 09:04:49 +0700

    menambah fungsi untuk candi jabug,plaosan,sewu,brahu dan banyunibo

--- story telling candi.js
+++ story telling candi.js
@@ -1,3 +1,65 @@
-<script>
-    
-</script>
+function jabug(){
+		document.getElementById("jabug").style.display = 'none';
+		document.getElementById("plaosan").style.display = 'none';
+		document.getElementById("sewu").style.display = 'none';
+		document.getElementById("brahu").style.display = 'none';
+		document.getElementById("banyunibo").style.display = 'none';
+		document.getElementById("candi jabug").style.display = 'block';
+		document.getElementById("candi plaosan").style.display = 'none';
+		document.getElementById("candi sewu").style.display = 'none';
+		document.getElementById("candi brahu").style.display = 'none';
+		document.getElementById("candi banyunibo").style.display = 'none';
+		event.preventDefault()
+	}
+	function plaosan(){
+		document.getElementById("jabug").style.display = 'none';
+		document.getElementById("plaosan").style.display = 'none';
+		document.getElementById("sewu").style.display = 'none';
+		document.getElementById("brahu").style.display = 'none';
+		document.getElementById("banyunibo").style.display = 'none';
+		document.getElementById("candi jabug").style.display = 'none';
+		document.getElementById("candi plaosan").style.display = 'block';
+		document.getElementById("candi sewu").style.display = 'none';
+		document.getElementById("candi brahu").style.display = 'none';
+		document.getElementById("candi banyunibo").style.display = 'none';
+		event.preventDefault()
+	}
+	function sewu(){
+		document.getElementById("jabug").style.display = 'none';
+		document.getElementById("plaosan").style.display = 'none';
+		document.getElementById("sewu").style.display = 'none';
+		document.getElementById("brahu").style.display = 'none';
+		document.getElementById("banyunibo").style.display = 'none';
+		document.getElementById("candi jabug").style.display = 'none';
+		document.getElementById("candi plaosan").style.display = 'none';
+		document.getElementById("candi sewu").style.display = 'block';
+		document.getElementById("candi brahu").style.display = 'none';
+		document.getElementById("candi banyunibo").style.display = 'none';
+		event.preventDefault()
+	}
+	function brahu(){
+		document.getElementById("jabug").style.display = 'none';
+		document.getElementById("plaosan").style.display = 'none';
+		document.getElementById("sewu").style.display = 'none';
+		document.getElementById("brahu").style.display = 'none';
+		document.getElementById("banyunibo").style.display = 'none';
+		document.getElementById("candi jabug").style.display = 'none';
+		document.getElementById("candi plaosan").style.display = 'none';
+		document.getElementById("candi sewu").style.display = 'none';
+		document.getElementById("candi brahu").style.display = 'block';
+		document.getElementById("candi banyunibo").style.display = 'none';
+		event.preventDefault()
+	}
+	function banyunibo(){
+		document.getElementById("jabug").style.display = 'none';
+		document.getElementById("plaosan").style.display = 'none';
+		document.getElementById("sewu").style.display = 'none';
+		document.getElementById("brahu").style.display = 'none';
+		document.getElementById("banyunibo").style.display = 'none';
+		document.getElementById("candi jabug").style.display = 'none';
+		document.getElementById("candi plaosan").style.display = 'none';
+		document.getElementById("candi sewu").style.display = 'none';
+		document.getElementById("candi brahu").style.display = 'none';
+		document.getElementById("candi banyunibo").style.display = 'block';
+		event.preventDefault()
+	}
